476e5fe0c35272cda383c306c618abf38de9d339
pannet1 <prog@ecomsense.in>
2025-06-24 20:55:55 +0530

    avg sell

--- src/strategies/fantastico.py
+++ src/strategies/fantastico.py
@@ -5,6 +5,16 @@ from toolkit.kokoo import timer
 import pendulum as pdlm
 
 COLS_DELIVERED = ["Symbol", "Qty", "Bdate", "Bprice", "Reward", "Ltp", "Exch"]
+HIST_COLS = [
+    "Symbol",
+    "Qty",
+    "Bdate",
+    "Bprice",
+    "Reward",
+    "SDate",
+    "SPrice",
+    "Exch",
+]
 REWARD_PERC = 0.05  # 5 percent
 
 
@@ -39,14 +49,10 @@ class Fantastico:
         print("\n DELIVERED \n", self.df_delivered, "\n")
         timer(2)
 
-        self.fn = (
-            self.exit_beyond_band
-            if self.df_stocks_in_play.empty
-            else self.enter_on_breakout
-        )
+        self.fn = self.process_cnc if self.df_stocks_in_play.empty else self.first_entry
 
-    def enter_on_breakout(self):
-        self.fn = self.exit_beyond_band
+    def first_entry(self):
+        self.fn = self.process_cnc
         try:
             self.df_stocks_in_play["Ltp"] = self.df_stocks_in_play.index.map(
                 self._prices
@@ -68,7 +74,7 @@ class Fantastico:
                         "Qty": int(row["Qty"]),
                         "Bdate": pdlm.now(),
                         "Bprice": Ltp,
-                        "Reward": Ltp + (Ltp * 4 / 100),
+                        "Reward": Ltp - (Ltp * REWARD_PERC),
                         "Ltp": Ltp,
                         "Symbol": idx,
                     }
@@ -94,7 +100,7 @@ class Fantastico:
         if not is_empty:
             self.df_delivered.to_csv(DELIVERED, index=False, header=True)
 
-    def add_position(self, **kwargs):
+    def _place_buy_order(self, **kwargs):
         resp = Helper.place_order(
             symbol=kwargs["Symbol"],
             exchange=kwargs["Exch"],
@@ -105,81 +111,109 @@ class Fantastico:
             return True
         return False
 
-    def exit_beyond_band(self):
-        try:
-            columns = [
-                "Symbol",
-                "Qty",
-                "Bdate",
-                "Bprice",
-                "Reward",
-                "SDate",
-                "SPrice",
-                "Exch",
-            ]
-            self.df_delivered = pd.read_csv(DELIVERED)
-            if not self.df_delivered.empty:
-                self.df_delivered["Ltp"] = self.df_delivered["Symbol"].map(self._prices)
-            for idx, row in self.df_delivered.iterrows():
-                rows_to_drop, rows_to_add, lst_of_dct = [], [], []
-                Ltp = row["Ltp"]
-                if Ltp >= row["Reward"]:
-                    resp = Helper.place_order(
-                        symbol=row["Symbol"],
-                        exchange=row["Exch"],
-                        quantity=row["Qty"],
-                        side="SELL",
-                    )
-                    if resp:
-                        dct = {
-                            "Symbol": row["Symbol"],
-                            "Qty": row["Qty"],
-                            "Bdate": row["Bdate"],
-                            "Bprice": row["Bprice"],
-                            "Reward": row["Reward"],
-                            "SDate": pdlm.now(),
-                            "SPrice": Ltp,
-                            "Exch": row["Exch"],
-                        }
-                        # to be added to the history
-                        lst_of_dct.append(dct)
-                        # to be dropped from delivered
-                        rows_to_drop.append(idx)
-                        dct = {
-                            "Exch": row["Exch"],
-                            "Qty": int(row["Qty"]),
-                            "Bdate": pdlm.now(),
-                            "Bprice": Ltp,
-                            "Reward": Ltp + (Ltp * REWARD_PERC),
-                            "Ltp": Ltp,
-                            "Symbol": row["Symbol"],
-                        }
-                        if self.add_position(**dct):
-                            # to be added to the df
-                            rows_to_add.append(dct)
-                            break
-                    elif Ltp < row["Bprice"] - (row["Bprice"] * REWARD_PERC):
-                        dct = {
-                            "Exch": row["Exch"],
-                            "Qty": int(row["Qty"]),
-                            "Bdate": pdlm.now(),
-                            "Bprice": Ltp,
-                            "Reward": Ltp + (Ltp * REWARD_PERC),
-                            "Ltp": Ltp,
-                            "Symbol": row["Symbol"],
-                        }
-                        if self.add_position(**dct):
-                            rows_to_add.append(dct)
-                            break
+    def remove_me(self):
+        # Create a copy with only last rows per symbol
+        df_latest = (
+            self.df_delivered.groupby("Symbol", as_index=False)
+            .tail(1)
+            .reset_index(drop=True)
+        )
+        for idx, row in df_latest.iterrows():
+            rows_to_drop, lst_of_dct = [], []
+            Ltp = row["Ltp"]
+            if Ltp >= row["Reward"]:
+                resp = Helper.place_order(
+                    symbol=row["Symbol"],
+                    exchange=row["Exch"],
+                    quantity=row["Qty"],
+                    side="SELL",
+                )
+                if resp:
+                    dct = {
+                        "Symbol": row["Symbol"],
+                        "Qty": row["Qty"],
+                        "Bdate": row["Bdate"],
+                        "Bprice": row["Bprice"],
+                        "Reward": row["Reward"],
+                        "SDate": pdlm.now(),
+                        "SPrice": Ltp,
+                        "Exch": row["Exch"],
+                    }
+                    # to be added to the history
+                    lst_of_dct.append(dct)
+                    # to be dropped from delivered
+                    rows_to_drop.append(idx)
 
             if lst_of_dct and rows_to_drop:
                 # if lst_of dct
-                df_new = pd.DataFrame(lst_of_dct, columns=columns)
+                df_new = pd.DataFrame(lst_of_dct, columns=HIST_COLS)
                 df_new.to_csv(HISTORY, mode="a", index=False, header=False)
                 # if rows_to_drop:
                 self.df_delivered.drop(rows_to_drop, inplace=True)
                 self.save_dfs()
 
+    def exit_on_profit(self, df: pd.DataFrame) -> bool:
+        symbol = df["Symbol"].iloc[0]  # all rows are of same symbol
+        avg_bprice = df["Bprice"].mean()
+        total_qty = df["Qty"].sum()
+        ltp = df["Ltp"].iloc[-1]
+        exch = df["Exch"].iloc[-1]
+
+        if ltp > avg_bprice:
+            resp = Helper.place_order(
+                symbol=symbol,
+                exchange=exch,
+                quantity=total_qty,
+                side="SELL",
+            )
+            if resp:
+                df["SDate"] = pdlm.now()
+                df["SPrice"] = ltp
+                df.to_csv(HISTORY, mode="a", index=False, header=False)
+                return True
+        return False
+
+    def enter_on_loss(self):
+        # Create a copy with only last rows per symbol
+        df_latest = (
+            self.df_delivered.groupby("Symbol", as_index=False)
+            .tail(1)
+            .reset_index(drop=True)
+        )
+        rows_to_add = []
+        for _, row in df_latest.iterrows():
+            Ltp = row["Ltp"]
+            if Ltp < row["Reward"]:
+                dct = {
+                    "Exch": row["Exch"],
+                    "Qty": int(row["Qty"]),
+                    "Bdate": pdlm.now(),
+                    "Bprice": Ltp,
+                    "Reward": Ltp + (Ltp * REWARD_PERC),
+                    "Ltp": Ltp,
+                    "Symbol": row["Symbol"],
+                }
+                if self._place_buy_order(**dct):
+                    rows_to_add.append(dct)
+                    return rows_to_add
+
+    def process_cnc(self):
+        try:
+            self.df_delivered = pd.read_csv(DELIVERED)
+            if not self.df_delivered.empty:
+                self.df_delivered["Ltp"] = self.df_delivered["Symbol"].map(self._prices)
+
+            symbols = self.df_delivered["Symbol"].unique()
+            for symbol in symbols:
+                df_sym = self.df_delivered[self.df_delivered["Symbol"] == symbol]
+                should_exit = self.exit_on_profit(df_sym)
+                if should_exit:
+                    self.df_delivered = self.df_delivered[
+                        self.df_delivered["Symbol"] != symbol
+                    ]
+                    self.save_dfs()
+
+            rows_to_add = self.enter_on_loss()
             if rows_to_add:
                 df_new_rows = pd.DataFrame(rows_to_add, columns=COLS_DELIVERED)
                 self.df_delivered = pd.concat(
@@ -187,7 +221,7 @@ class Fantastico:
                 )
                 self.save_dfs()
         except Exception as e:
-            print(f"{e} exit beyond band")
+            print(f"{e} process cns")
 
     def run(self, prices):
         # update df stocks in play and df_delivered with prices
